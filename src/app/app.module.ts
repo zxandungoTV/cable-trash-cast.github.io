251c0da2674faf0e7fd5c39ddd54be330da19dfe
zxanderrodriguez <zxanderrodriguez@gmail.com>
2021-09-13 13:20:52 -0500

    List changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { EpisodePageComponent } from './episode-page/episode-page.component';
 import { MembersPageComponent } from './members-page/members-page.component';
 import { MerchPageComponent } from './merch-page/merch-page.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @NgModule({
   declarations: [
@@ -22,7 +31,14 @@ import { ErrorPageComponent } from './error-page/error-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    CdkAccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/episode-page/episode-page.component.ts
+++ src/app/episode-page/episode-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-episode-page',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodePageComponent implements OnInit {
 
-  constructor() { }
+  items = [
+    {
+      name: ' Ep(1) - "Stop Motion"',
+      description: `Me and some buddies talk about stop motion stuff for a while.`,
+      youtubeLink: this.dom.bypassSecurityTrustResourceUrl("https://www.youtube-nocookie.com/embed/UuaypCQ1Bbc")
+    },
+    {
+      name: 'Ep(2) - "Puppets"',
+      description: `Me and some friends talking about puppet stuff for a few hours. Sorry for the bad audio quality on this one`,
+      youtubeLink: this.dom.bypassSecurityTrustResourceUrl("https://www.youtube-nocookie.com/embed/-N2_LHhYc4I")
+    },
+    {
+      name: 'Ep(3) - "B - Movies"',
+      description: `Talking about some of our favorite B movies, or at least the one we have seen.`,
+      youtubeLink: this.dom.bypassSecurityTrustResourceUrl("https://www.youtube-nocookie.com/embed/nYYDq8AwHVE")
+    },
+    {
+      name: 'Ep(4) - "Live Action Cartoon Hybrids"',
+      description: `On today episode the gang talks about movies that introduces the live world interacting with the cartoon one.`,
+      youtubeLink: this.dom.bypassSecurityTrustResourceUrl("https://www.youtube-nocookie.com/embed/rC6dhhde3vQ")
+    }];
+  expandedIndex = 0;
+  constructor(private dom: DomSanitizer) { }
 
   ngOnInit(): void {
   }
